6141354d56b7c0a5b033e5bb6bfd56595b00fc6f
Mohammed Maqsood shaik <shaikmdmaqsood@gmail.com>
2023-01-09 13:58:00 +0100

    working

--- lstm_lang_mode.py
+++ lstm_lang_mode.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torchvision
 import torchvision.transforms as transforms
 import pandas as pd
 from datasets import Dataset
@@ -17,140 +16,130 @@ def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 set_seed(0)
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
 
+g = torch.Generator()
+g.manual_seed(0)
 import wandb
 # 1. Start a W&B run
-wandb.init(project='lstm_langmodel')
+wandb.init(project='lstm_langmodel_batch')
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyper-parameters 
-# input_size = 784 # 28x28
-num_classes = 6
-num_epochs = 500
-batch_size = 32
+
+
+
 config = wandb.config
-config.batch_size = batch_size
+config.batch_size = 16
+config.num_epochs = 500
 # 2. Save model inputs and hyperparameters
 config = wandb.config
-learning_rate = 0.00005
-config.learning_rate = learning_rate
+config.learning_rate = 1e-3
 config.one_hot = True
-input_size = 640
-sequence_length = 560
-hidden_size = 1024
-config.hidden_size = hidden_size
-num_layers = 2
-config.num_layers  = num_layers
-
+config.hidden_size = 1024
+config.num_layers  = 2
 # MNIST dataset 
-def data_extract( dataset_path): 
-    feature_extractor = AutoFeatureExtractor.from_pretrained("facebook/wav2vec2-base")
-    model = Wav2Vec2ForPreTraining.from_pretrained("facebook/wav2vec2-base")
-    accuracy = 0
-    num_files = 0
-    pred_languages = []
-    actual_languages= []
-    cf_prb_m = {}
-    #empty data frame with column names audio_embedding and language
-    df_lang = pd.DataFrame(columns=['audio_embedding', 'language'])
-    for subdir, dirs, files in os.walk(dataset_path):
-        for file in files:
-            if file == "test.csv":
-                #reading the csv file
-                df = pd.read_csv(subdir + "/" + file)
-                #read the first column of the csv file
-                df = df.iloc[:, 0].values
-                for filename in df[:1000]:
-                    audio_path = subdir + "/wav/" + filename.split('.')[0]+".wav"
-                    codebook = 1
-                    mdl_out_cd_1 = model_output(audio_path = audio_path,codebook = codebook,feature_extractor = feature_extractor,model=model)
-                    codebook = 2
-                    mdl_out_cd_2 = model_output(audio_path = audio_path,codebook = codebook,feature_extractor = feature_extractor,model=model)
-                    mdl_out = mdl_out_cd_1+np.array(np.array(mdl_out_cd_2)+320).tolist()
-                    label = subdir.split('/')[-1]
-                    #append the audio embedding and language to the dataframe
-                    df_lang = df_lang.append({'audio_embedding': torch.tensor(mdl_out), 'language': label}, ignore_index=True)
-    return df_lang
 lang = ["de","en","es","fr","it","ru"]
-df =  pd.DataFrame(columns=['audio_embedding', 'language'])
-#pickle file exists
-if os.path.exists("lang_data.pkl"):
-    df = pd.read_pickle("lang_data.pkl")
-else:
-    for l in lang:
-        df_lang = data_extract(dataset_path = f"/corpora/common_phone/{l}")
-        #append df_lang to df
-        breakpoint()
-        df = df.append(df_lang, ignore_index=True)
-    #save the dataframe to pickle file
-    df.to_pickle("lang_data.pkl")
-#dataset from dataframe
-
+num_classes = len(lang)
+model_checkpoint = "facebook/wav2vec2-large-xlsr-53"
 label2id, id2label,label2id_int = dict(), dict(),dict()
 for i, label in enumerate(lang):
     label2id[label] = torch.tensor(i)
     id2label[torch.tensor(i)] = label
     label2id_int[label] = i
-#get the audio embedding and language from the dataframe and convert it to list
-audio_embedding = df['audio_embedding'].tolist()
-#ValueError: only one element tensors can be converted to Python scalars
-#convert the list with each element of different sizes to torch tensor
+def data_extract(model_checkpoint, dataset_path_m,file_to_extract): 
+    feature_extractor = AutoFeatureExtractor.from_pretrained(model_checkpoint)
+    model = Wav2Vec2ForPreTraining.from_pretrained(model_checkpoint)
+    for l in lang:
+        dataset_path = dataset_path_m + l + "/"
+        #read the csv file first column using the path dataset_path + "/" + file_to_extract to get the path of the audio file ignoreing the first row
+        for audio_file_path in pd.read_csv(dataset_path + "/" + file_to_extract, header=None)[0][1:]:
+            audio_path = dataset_path + "/wav/" + audio_file_path.split('.')[0]+".wav"
+            codebook = 1
+            mdl_out_cd_1 = model_output(audio_path = audio_path,codebook = codebook,feature_extractor = feature_extractor,model=model)
+            codebook = 2
+            mdl_out_cd_2 = model_output(audio_path = audio_path,codebook = codebook,feature_extractor = feature_extractor,model=model)
+            mdl_out = mdl_out_cd_1+np.array(np.array(mdl_out_cd_2)+320).tolist()
+            #yield dictioanry with the audio feature and the label
+            yield {"input_values":torch.tensor(mdl_out).unsqueeze(dim = 0),"labels":torch.tensor(label2id[l]).unsqueeze(dim = 0)}
+#load_from_disk if train_dataset contains the data
+if os.path.exists("train_dataset")and os.path.exists("val_dataset") :
+    train_dataset = Dataset.load_from_disk("train_dataset")
+    val_dataset = Dataset.load_from_disk("val_dataset")
+    # test_dataset = Dataset.load_from_disk("test_dataset")
+else:
+    file_to_extract = "train.csv"
+    # Create a new function that always uses the value 'hello' for arg1
+    train_dataset= Dataset.from_generator(data_extract,gen_kwargs={"model_checkpoint":model_checkpoint,"dataset_path_m":f"/corpora/common_phone/","file_to_extract":file_to_extract},cache_dir = "/cache")
+                                        #,output_types={"input_values":torch.tensor,"labels":torch.tensor})
+    # file_to_extract = "dev.csv"
+    # val_dataset = Dataset.from_generator(data_extract,gen_kwargs={"model_checkpoint":model_checkpoint,"dataset_path_m":f"/corpora/common_phone/","file_to_extract":file_to_extract},cache_dir = "/cache")
+    file_to_extract = "test.csv"
+    val_dataset = Dataset.from_generator(data_extract,gen_kwargs={"model_checkpoint":model_checkpoint,"dataset_path_m":f"/corpora/common_phone/","file_to_extract":file_to_extract},cache_dir = "/cache")
+    #save the dataset in optimal format by using the save_to_disk function with folders created
+    #create the folders before saving the dataset
+    os.makedirs("train_dataset", exist_ok=True)
+    train_dataset.save_to_disk("./train_dataset")
+    os.makedirs("val_dataset", exist_ok=True)
+    val_dataset.save_to_disk("./val_dataset")
+    # os.makedirs("test_dataset", exist_ok=True)
+    # test_dataset.save_to_disk("./test_dataset")
 
-language = df['language'].tolist()
-
-
-language = [label2id[l] for l in language]
-language = torch.tensor(language)
-# #create one hot encoding for the language
-# language = torch.nn.functional.one_hot(language, num_classes=6)
-#convert dtype of language to torch.float32
-language = language.type(torch.float32)
-
-#split the audio_embedding language and into train and test
-train_dataset_x, test_dataset_x,train_dataset_y, test_dataset_y = train_test_split(audio_embedding, language, test_size=0.2, random_state=42)
-#dataset for train and test
-print(len(train_dataset_x))
-print(len(test_dataset_x))
-# train_dataset = Dataset(train_dataset_x,train_dataset_y)
-# test_dataset = Dataset(test_dataset_x,test_dataset_y)
-
-# Data loader
-# train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-#                                            batch_size=batch_size, 
-#                                            shuffle=True)
+#creater the dataloader for the train and test dataset with each having same length using the collate_fn
+def collate_fn(batch):
+    input_values = [item["input_values"] for item in batch]
+    labels = [item["labels"] for item in batch]
+    input_values = [torch.tensor(i).squeeze() for i in input_values]
+    input_values = torch.nn.utils.rnn.pad_sequence(input_values, batch_first=True,padding_value=640)
+    return {"input_values":input_values,"labels":torch.tensor(labels).squeeze()}
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=config.batch_size, 
+                                           shuffle=True,collate_fn=collate_fn,drop_last=True,worker_init_fn=seed_worker,generator=g,)
 
 # test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
 #                                           batch_size=batch_size, 
 #                                           shuffle=False)
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
+                                            batch_size=config.batch_size,
+                                            shuffle=False,collate_fn=collate_fn,drop_last=Trueworker_init_fn=seed_worker,generator=g,)
 
 
 # Fully connected neural network with one hidden layer
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+    def __init__(self,  hidden_size, num_layers, num_classes,ntoken, ninp):
         super(RNN, self).__init__()
+        self.encoder = nn.Embedding(ntoken, ninp, padding_idx=ntoken-1)
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         # self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
         # -> x needs to be: (batch_size, seq, input_size)
         
         # or:
-        #self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
+        self.gru = nn.GRU(ninp, hidden_size, num_layers, batch_first=True)
+        # self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc1 = nn.Linear(hidden_size, int(hidden_size/2))
+        self.fc2 = nn.Linear(int(hidden_size/2), num_classes)
+        
         
     def forward(self, x):
         # Set initial hidden states (and cell states for LSTM)
+        x = self.encoder(x)
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
+        # c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
         
         # x: (n, 28, 28), h0: (2, n, 128)
         
         # Forward propagate RNN
-        # out, _ = self.rnn(x, h0)  
+        out, _ = self.gru(x, h0)  
         # or:
-        out, _ = self.lstm(x, (h0,c0))  
+        # out, _ = self.lstm(x, (h0,c0))  
         
         # out: tensor of shape (batch_size, seq_length, hidden_size)
         # out: (n, 28, 128)
@@ -158,82 +147,89 @@ class RNN(nn.Module):
         # Decode the hidden state of the last time step
         out = out[:, -1, :]
         # out: (n, 128)
-         
-        out = self.fc(out)
+        out = torch.nn.functional.relu(out)
+        out = self.fc1(out)
+        out = torch.nn.functional.relu(out)
+        out = self.fc2(out)
         # out: (n, 10)
         return out
-
-model = RNN(input_size, hidden_size, num_layers, num_classes).to(device)
+from accelerate import Accelerator
+accelerator = Accelerator()
+print(torch.cuda.device_count())
+print(accelerator.state.num_processes)
+config.input_size = 641
+ninp = 512
+model = RNN( config.hidden_size, config.num_layers, num_classes,config.input_size, ninp).to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
+optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
+model, optimizer, train_loader,val_loader  = accelerator.prepare(
+    model, optimizer, train_loader,val_loader
+)
 best_acc = 0
 lr_scheduler = get_scheduler(
         name="linear",
         optimizer=optimizer,
-        num_warmup_steps=int(num_epochs*(len(train_dataset_y)/batch_size)*0.1),
-        num_training_steps=num_epochs*(len(train_dataset_y)/batch_size),
+        num_warmup_steps=int(config.num_epochs*(len(train_loader))*0.1),
+        num_training_steps=config.num_epochs*(len(train_loader)),
     )
-c = list(zip(train_dataset_x, train_dataset_y))
 # Train the model
-n_total_steps = len(train_dataset_y)
-for epoch in range(num_epochs):
-    random.shuffle(c)
-    train_dataset_x, train_dataset_y = zip(*c)
+n_total_steps = len(train_loader)
+for epoch in range(config.num_epochs):
     loss_epoch =0
-    loss_batch = 0
-    for i, labels in enumerate(train_dataset_y):  
+    for i, values in enumerate(train_loader):  
         # origin shape: [N, 1, 28, 28]
-        # resized: [N, 28, 28]
-        #one hot encoding for the train_dataset_x last dimension
-        inp = torch.nn.functional.one_hot(train_dataset_x[i], num_classes=input_size)
-            
-        images = torch.tensor(inp,dtype=torch.float32).reshape(1,-1,input_size).to(device)
-        labels= labels.unsqueeze(0)
-        labels= labels.type(torch.LongTensor).to(device)
+        # images = torch.nn.functional.one_hot(values["input_values"], num_classes=641).to(device)
+        #set images dtype to float32
+        # images = values["input_values"].type(torch.float32)
+        images = values["input_values"].to(device,dtype=torch.long)
+        # images = values["input_values"].to(device)
+        labels= values["labels"]
+        labels = labels.type(torch.LongTensor).to(device)
         # Forward pass
-    
+
         outputs = model(images)
-        loss = criterion(outputs,labels )
+        loss = criterion(outputs,labels )       
+        # Backward and optimize
+        optimizer.zero_grad()
+        # loss.backward()
+        accelerator.backward(loss)
+        optimizer.step()
+        lr_scheduler.step()
         
-        #accumaulate the loss into a variable for printing after each epoch
-        loss_epoch += loss.item()
-        loss_batch += loss
-        if (i+1) % batch_size == 0:
-                   
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss_batch.backward()
-            optimizer.step()
-            lr_scheduler.step()
-            loss_batch = 0 
-        
-        if (i+1) % 1000 == 0:
-            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+        loss_epoch += accelerator.gather(loss)
+        if accelerator.is_main_process:
+            if (i+1) % 1000 == 0:
+                print (f'Epoch [{epoch+1}/{config.num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
     # Test the model
     # In test phase, we don't need to compute gradients (for memory efficiency)
-    wandb.log({"loss": loss_epoch/len(train_dataset_y)})
-    print("loss per epoch",loss_epoch/len(train_dataset_y))
-    with torch.no_grad():
-        n_correct = 0
-        n_samples = 0
-        for i, labels in enumerate(test_dataset_y):
-            inp = torch.nn.functional.one_hot(test_dataset_x[i], num_classes=input_size)
-            images =torch.tensor(inp,dtype=torch.float32).reshape(1,-1,input_size).to(device)
-            labels = labels.to(device)
-            labels= labels.type(torch.LongTensor)
-            outputs = model(images)
-            # max returns (value ,index)
-            _, predicted = torch.max(outputs.data, 1)
-            n_samples += 1
-            n_correct += (predicted == labels).sum().item()
-        acc = 100.0 * n_correct / n_samples
-        wandb.log({"validation accuracy": acc})
-        print(f'Accuracy of the network on the test images: {acc} %')
-        if acc > best_acc:
-            best_acc = acc
-            torch.save(model.state_dict(), 'best_model.pt')
-            print('model saved')
-            print(f'Best Accuracy of the network on the test images: {best_acc} %')
+    if accelerator.is_main_process:
+        wandb.log({"loss": loss_epoch.sum()/(len(loss_epoch)*len(train_loader))})
+        print("loss per epoch",loss_epoch.sum()/(len(loss_epoch)*len(train_loader)))
+        with torch.no_grad():
+            n_correct = 0
+            n_samples = 0
+            val_loss = 0
+            for i, values in enumerate(val_loader):
+                # images = torch.nn.functional.one_hot(values["input_values"], num_classes=641).to(device)
+                # images = images.type(torch.float32)
+                images = values["input_values"].to(device,dtype=torch.long)
+                labels = values["labels"]
+                labels = labels.type(torch.LongTensor).to(device)
+                outputs = model(images)
+                loss = criterion(outputs,labels )
+                val_loss += loss.item()
+                # max returns (value ,index)
+                _, predicted = torch.max(outputs.data, 1)
+                n_correct += (predicted == labels).sum()
+            acc = (100.0 * n_correct )/ (len(val_loader)*config.batch_size)
+            wandb.log({"validation accuracy": acc})
+            wandb.log({"validation loss": val_loss/(len(val_loader))})
+            print(f'Accuracy of the network on the test images: {acc} %')
+            if acc > best_acc:
+                best_acc = acc
+                torch.save(model.state_dict(), 'best_model_lstm_batch.pt')
+                print('model saved')
+                print(f'Best Accuracy of the network on the test images: {best_acc} %')
